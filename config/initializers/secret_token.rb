692df3e34863d2385f25986cbd0b8014afba63fb
Sathish <sathish316@gmail.com>
2012-02-13 20:45:03 +0530

    Initial commit: generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anyscp::Application.config.secret_token = '849ef920f41a5be84d63cece1155175c274b800b4784fccdfcd801c59af0e9c86dc9b0036a83fdf6bce0400d3b23823db91dd2f9d44546cccb6778d5878908d3'
